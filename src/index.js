5c23f4d20c6356e31bbfc9a77f2f7bac8dcbe23e
selaaulia <selaaulia1307@gmail.com>
2022-02-24 13:43:55 +0700

    Pertemuan 2

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Hello = () => {
+  //return <p>Hello</p>;
+  return <div>
+  <header className="Main-page">
+    <h1>
+      <marquee direction="right">Hello Sela Aulia Siswanto !!! Selamat Datang di React JS</marquee>    
+    </h1>
+  </header>
+</div>
+};
+
+ReactDOM.render(<Hello />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
